11d710369c4695f917a77d9fbda10e5e1e0b8655
Codrut Grosu <codrut.cristian.grosu@gmail.com>
2017-05-01 15:13:49 +0300

    awareness: headphone: successfully got the headphone state
    Signed-off-by: Codrut Grosu <codrut.cristian.grosu@gmail.com>

--- app/src/main/java/cs/ai/upbassistant/AwarenessBackgroundService.java
+++ app/src/main/java/cs/ai/upbassistant/AwarenessBackgroundService.java
@@ -1,28 +1,72 @@
 package cs.ai.upbassistant;
 
+import android.animation.ObjectAnimator;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.util.Log;
+import com.google.android.gms.awareness.Awareness;
+import com.google.android.gms.awareness.snapshot.HeadphoneStateResult;
+import com.google.android.gms.awareness.state.HeadphoneState;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 
 
 public class AwarenessBackgroundService extends IntentService {
 
+    private GoogleApiClient googleApiClient;
     private final String TAG = "AwarenessBackgroundService";
+    public static Object objHeadphone = new Object();
     public AwarenessBackgroundService() {
         super("AwarenessBackgroundService");
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        setupGoogleApiClient();
+        writeHeadphoneState();
+    }
+
+    public void setupGoogleApiClient() {
         Context context = this.getApplicationContext();
-        for(Integer i = 1; i < 6; i ++ ) {
+        googleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(Awareness.API)
+                .build();
+        googleApiClient.connect();
+    }
+
+    public void writeHeadphoneState() {
+        PendingResult<HeadphoneStateResult> result1 = Awareness.SnapshotApi
+                .getHeadphoneState(googleApiClient);
+        result1.setResultCallback(new ResultCallback<HeadphoneStateResult>() {
+            @Override
+            public void onResult(@NonNull HeadphoneStateResult headphoneStateResult) {
+                if (!headphoneStateResult.getStatus().isSuccess()) {
+                    Log.e(TAG, "Could not get headphone state.");
+                    return;
+                }
+                HeadphoneState headphoneState = headphoneStateResult.getHeadphoneState();
+                if (headphoneState.getState() == HeadphoneState.PLUGGED_IN) {
+                    Log.i(TAG, "Headphones are plugged in.\n");
+                }
+                else {
+                    Log.i(TAG, "Headphones are NOT plugged in.\n");
+                }
+                synchronized (AwarenessBackgroundService.objHeadphone) {
+                    AwarenessBackgroundService.objHeadphone.notifyAll();
+                }
+            }
+        });
+        synchronized (this.objHeadphone) {
             try {
-                Thread.sleep(1000);
-                Log.d(TAG, i.toString());
+                this.objHeadphone.wait();
             } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+                Log.e(TAG,  "Interrupted exception " + e.getMessage());
             }
         }
     }
+
+
 }
